47299c9396edff899c357fd068f5de6ab012626a
RatSteak <ratsteak@protonmail.com>
2020-04-20 13:15:13 +0200

    Shell script to update Firefox in Debian

---
+++ DebianUpdateFirefox/update_firefox.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /tmp
+wget -O FirefoxSetup.tar.bz2 "https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=es-ES"
+tar xjf FirefoxSetup.tar.bz2 -C /opt
+unlink /usr/bin/firefox
+ln -s /opt/firefox/firefox-bin /usr/bin/firefox
+rm FirefoxSetup.tar.bz2
