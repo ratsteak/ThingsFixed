42253b6a1cbaf13aa961b7e6826d0d738facba85
RatSteak <ratsteak@protonmail.com>
2020-04-20 13:20:21 +0200

    Shell script to update Thunderbird in Debian

---
+++ DebianUpdateFirefox/update_thunderbird.sh
@@ -0,0 +1,9 @@
+!#/bin/bash
+cd /tmp
+wget -O Thunderbird.tar.bz2 "https://download.mozilla.org/?product=thunderbird-latest&os=linux64&lang=es-ES"
+tar xjf Thunderbird.tar.bz2 -C /opt
+unlink /usr/bin/thunderbird
+ln -s /opt/thunderbird/thunderbird-bin /usr/bin/thunderbird
+rm Thunderbird.tar.bz2
+
+
